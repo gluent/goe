af5cc12e1e62251d9a1e203edfaaf9a9567fd848
abb9979 <adrian.billington@gmail.com>
2023-12-06 16:36:58 +0000

    refactor: GOE repository model and Oracle install/upgrade scripts

--- sql/oracle/source/sql/create_offload_repo_privs.sql
+++ sql/oracle/source/sql/create_offload_repo_privs.sql
@@ -7,4 +7,3 @@ set serveroutput on
 
 prompt Granting system privileges for &goe_db_repo_user user...
 GRANT CREATE SESSION TO &goe_db_repo_user.;
-GRANT SELECT ANY DICTIONARY TO &goe_db_repo_user.;

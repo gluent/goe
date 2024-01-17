ec49cd5ec979507faf6f70772f7a638b9f989533
nj1973 <neil@thirdchimp.net>
2024-01-17 17:16:25 +0000

    chore: Remove some redundant test files

--- testing-to-remove/environments/oracle/gluent_demo/demo_videos/my_plsql_func.sql
+++
@@ -1,6 +0,0 @@
-create or replace function sh.my_plsql_func(p in varchar2) return varchar2 is
-begin
-  return p;
-end my_plsql_func;
-/
-
--- testing-to-remove/environments/oracle/gluent_demo/documentation/performance_screenshots/reinstate_non_materialized_sales_costs_join.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This reinstates the non-materialized sales_costs_join view and renames the materialized 
-# version for safekeeping. Assumes the original present/offload scripts were run.
-
-impala-shell -q "alter table sales_costs_join rename to sales_costs_join_mat"
-
-./present_sales_costs_join.sh
